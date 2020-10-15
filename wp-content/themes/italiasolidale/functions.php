1ee93f6b8dca1f7b13718ebdf7273c492ce6e37b
giorgiocolombo <57524487+giorgiocolombo@users.noreply.github.com>
2020-10-15 23:32:24 +0200

    last

--- wp-content/themes/italiasolidale/functions.php
+++ wp-content/themes/italiasolidale/functions.php
@@ -53,15 +53,12 @@ function ITAS_allowed_block_types() {
 		'core/paragraph',
 		'core/heading',
         'core/buttons',
-        'core/shortcode',
         'core-embed/youtube',
         'jetpack/slideshow',
         'webfactory/map',
         'acf/chosen-products',
         'acf/block-left-img',
-        'acf/block-right-img',
-        'acf/contacts'
-        
+        'acf/block-right-img'        
 	);
     
 }
@@ -98,16 +95,6 @@ function ITAS_acf_init_block_types() {
             'category'          => 'formatting',
             'icon'              => 'align-right',
             'keywords'          => array( 'block-right-img', 'quote' ),
-        ));
-
-        acf_register_block_type(array(
-            'name'              => 'contacts',
-            'title'             => __('Blocco contatti'),
-            'description'       => __('Mostra un blocco con i contatti'),
-            'render_template'   => 'template_parts/blocks/contacts/contacts.php',
-            'category'          => 'design',
-            'icon'              => 'email',
-            'keywords'          => array( 'contacts', 'quote' ),
         ));
     }
 }
